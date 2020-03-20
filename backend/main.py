3a6d6e9c623bd3a8a65e0d7538440d990290c161
Quan Nguyen <quanguyen250@gmail.com>
2020-03-20 16:33:41 -0400

    Moved file

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Comment</h1>"
+    return flask.render_template("index.html", token="Using React + Flask + SQLalchemy")
 
 if __name__ == "__main__":
   app.run(debug=True)
