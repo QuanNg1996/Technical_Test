0a5ef6b21700d4186e6163ac9ef2a57dea12dc4a
Quan Nguyen <quanguyen250@gmail.com>
2020-03-20 17:03:43 -0400

    Python3

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__main__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return flask.render_template("index.html", token="Using React + Flask + SQLalchemy")
+  return flask.render_template("index.html", token="Using React + Flask + SQLalchemy")
 
-if __name__ == "__main__":
-  app.run(debug=True)
+app.run(debug=True)
