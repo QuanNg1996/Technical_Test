0428344b45deb2a3ff9059ace69392dd06e6e30d
Quan Nguyen <quanguyen250@gmail.com>
2020-03-20 17:07:17 -0400

    env

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
-import flask
+from flask import Flask
 
-app = flask.Flask(__main__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
   return flask.render_template("index.html", token="Using React + Flask + SQLalchemy")
 
-app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
