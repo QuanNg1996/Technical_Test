2eefa3fc19e8a2fa5603db635d11db941c1a9c1f
Quan Nguyen <quanguyen250@gmail.com>
2020-03-20 16:59:19 -0400

    Connected React & Flask

---
+++ backend/static/react/precache-manifest.4b84e31bfb1283ce6797fc5765cc2927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc62e6466d0d14324da21eabe55d81cc",
+    "url": "/static/react/../../template/index.html"
+  },
+  {
+    "revision": "5f08b95fc6594685b778",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7d2c34b4aaa69660d9f2",
+    "url": "/static/react/js/2.b70243af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.b70243af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f08b95fc6594685b778",
+    "url": "/static/react/js/main.17877ee1.chunk.js"
+  },
+  {
+    "revision": "ffee4126a3362f0bbbe6",
+    "url": "/static/react/js/runtime-main.cebc7f46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
